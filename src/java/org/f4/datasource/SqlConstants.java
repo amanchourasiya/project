53b0915bdf1087e2bd713a0d177c52ca1e05e72a
jarvis <jarvis@jarvis-the-boss>
2017-04-05 00:05:01 +0530

    Added bean specification

---
+++ src/java/org/f4/datasource/SqlConstants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.f4.datasource;
+
+/**
+ *
+ * @author jarvis
+ */
+public class SqlConstants {
+    public static final String LOGIN_CHECK="";
+    public static final String STUDENT_REGISTER="";
+    public static final String TEACHER_REGISTER="";
+    
+    
+}
